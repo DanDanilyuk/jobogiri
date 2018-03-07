6e4d59b6e4a5de083fdc6073beae02ff32e25fd9
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-03-07 18:52:18 -0500

    add routes for user_jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'jobs/index'
+  devise_for :users
+  get 'search', to: 'jobs#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'jobs#home'
   resources :jobs
+  resources :user_jobs
 end
