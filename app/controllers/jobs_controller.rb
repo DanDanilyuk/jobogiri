83d1071d2abeba702c54a4db0d4082eea7730804
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-03-07 21:37:12 -0500

    add flash messages

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,15 @@
 class JobsController < ApplicationController
   def home
+    flash[:success] = "Welcome to the Sample App!"
     @jobs = Job.all
-    # ScrapeIndeedJob.perform_now
+  end
+
+  def index
+    @jobs = Job.all
+  end
+
+  def search
+    ScrapeIndeedJob.perform_now
+    redirect_to root_path, flash: {success: "Success! Job Added to your jobs."}
   end
 end
