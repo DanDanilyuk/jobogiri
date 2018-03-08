a047f2157bdf89a8e2d835a96e3e4f1e72a766b5
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-03-07 21:43:51 -0500

    add job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,6 @@
 class Job < ApplicationRecord
+  # validates_presence_of :name, :location, :company, :body, :link
+  validates_uniqueness_of :link
+  has_many :user_jobs
+  has_many :users, through: :user_jobs
 end
