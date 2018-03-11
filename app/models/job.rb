1a6864345586dc2b1fe7d139bfb113fbd433fc08
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-03-10 21:17:57 -0500

    add state enum to job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,6 @@
 class Job < ApplicationRecord
   # validates_presence_of :name, :location, :company, :body, :link
+  enum state: [:active, :removed]
   validates_uniqueness_of :link
   has_many :user_jobs
   has_many :users, through: :user_jobs
