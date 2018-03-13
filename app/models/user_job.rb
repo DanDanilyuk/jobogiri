7d389f55a960606f2135067de56394049a95b95e
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-03-13 18:02:24 -0400

    add uniqueness for duplicate jobs

--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -2,4 +2,6 @@ class UserJob < ApplicationRecord
   belongs_to :user
   belongs_to :job
   enum state: [:added, :applied, :responded, :accepted, :denied]
+  validates :user_id, uniqueness: { scope: :job_id,
+    message: "You cant favorite the same job!" }
 end
