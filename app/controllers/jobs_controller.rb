bab5b60b904a446d2c8782d82072c507cb8422e5
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-03-17 00:15:34 -0400

    test commit for aws

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   def home
-    # flash[:success] = "Hey Welcome to the Sample App!"
+    # flash[:success] = "Welcome to the Sample App!"
     @jobs = Job.where(state: 0)
   end
 
