156c6af61d4c2ca64fa3acba4ab91d1f78b5eb20
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-03-18 16:28:34 -0400

    add validation for unique emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_uniqueness_of :email
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   has_many :user_jobs
   has_many :jobs, through: :user_jobs
 end
