e633272f0ecb6fc86fe7d67a847df1834e1732db
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-03-26 15:36:51 -0400

    update schedule to cron

--- config/initializers/shedule.rb
+++ config/initializers/shedule.rb
@@ -3,6 +3,6 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler::singleton
 
 # Proper Scraper Schedule
-scheduler.chron '1 6-20 * * *' do
+scheduler.cron '1 6-20 * * *' do
   ScrapeIndeedJob.perform_now
 end
