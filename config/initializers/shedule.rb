5b2df17645d18858cd232a2e710df003c1ddfef4
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-04-05 19:11:51 -0400

    add shedule for check_jobs every 6 hours

--- config/initializers/shedule.rb
+++ config/initializers/shedule.rb
@@ -3,6 +3,10 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler::singleton
 
 # Proper Scraper Schedule
-scheduler.cron '1 6-20 * * *' do
+scheduler.cron '*/20 6-20 * * *' do
   ScrapeIndeedJob.perform_now
+end
+
+scheduler.cron '1 6-20/6 * * *' do
+  CheckJobsJob.perform_now
 end
