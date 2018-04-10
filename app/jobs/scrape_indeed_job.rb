59eff29ee5bfd3b0db91e93091e3b2b96de0c57f
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-04-10 11:59:10 -0400

    change scraper body encoding

--- app/jobs/scrape_indeed_job.rb
+++ app/jobs/scrape_indeed_job.rb
@@ -26,7 +26,7 @@ class ScrapeIndeedJob < ApplicationJob
               posting_name = doc2.xpath("//b[@class='jobtitle']")[0].children.children.text
               posting_location = doc2.xpath("//input[@id='where']/@value").text
               posting_company = doc2.xpath("//td[1]/div/span[@class='company']").text
-              posting_body = doc2.xpath("//span[@id='job_summary']").to_html
+              posting_body = doc2.xpath("//span[@id='job_summary']").to_html.encode('CP1252').force_encoding('UTF-8')
               Job.create(name: posting_name, location: posting_location, company: posting_company, body: posting_body, link: link)
               p Job.count.to_s + ' Job Models Succesfully Created'
               duplicates = 0
